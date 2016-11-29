75c19d77f1fad8b3a9f2db6694beded33a852a52
Joseph Murphy <jmrphyii@memphis.edu>
2016-11-28 23:10:06 -0600

    fix follow

--- app/helpers/follows_helper.rb
+++ app/helpers/follows_helper.rb
@@ -1,14 +1,11 @@
 module FollowsHelper
-  def find_user(user)
-    user = User.where(email: user.email).first
-    #id = user.id
-    return user.id
-  end
+
   def follow_rec(current_user, follows)
     follow = Follow.where('user_id = ? AND follows = ?',current_user.id,follows.id).first
     return follow.id
   end
-  def gen_name(f,l)
-    return f + " " + l
+
+  def user_follows(current_user, user)
+       return Follow.find_by(user_id: current_user, follows: user)
   end
 end
