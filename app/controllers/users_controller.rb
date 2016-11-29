2c3d2d4368d0d80064a4378d4566328276726acb
Joseph Murphy <jmrphyii@memphis.edu>
2016-11-28 23:56:59 -0600

    update user profiles and the corresponding controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = User.all
-    @posts = Post.where(user_id: current_user.id)
+    @posts = Post.where(user_id: params[:id])
   end
 end
