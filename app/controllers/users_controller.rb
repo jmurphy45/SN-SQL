672265df74c99176203bc38b096e831b859cfc14
Joseph Murphy <jmrphyii@memphis.edu>
2016-11-29 10:09:09 -0600

    add who user follows to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @users = User.all
     @posts = Post.where(user_id: params[:id])
+    @who_follows = follows(params[:id])
+  end
+
+  def follows(user)
+    @follows = Follow.where(user_id: user)
+    @who = []
+    @follows.each do |f|
+      @who.push(User.find(f.follows))
+    end
+    return @who
   end
 end
